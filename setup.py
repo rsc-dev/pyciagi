9c351d4621574a2533131cf1fc9958a7d829f5c5
rsc-dev <roscoe@o2.pl>
2015-06-21 19:42:15 +0200

    Changes for PyPI.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pyciagi',
+  packages = ['pyciagi'],
+  version = '1.0.0',
+  description = 'Non-official API for real-time trains position across Poland.',
+  author = 'rsc',
+  author_email = 'radoslawmatusiak@gmail.com',
+  url = 'https://github.com/rsc-dev/pyciagi', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['pociagi', 'trains', 'Poland', 'Polska'],
+  classifiers = [],
+)
